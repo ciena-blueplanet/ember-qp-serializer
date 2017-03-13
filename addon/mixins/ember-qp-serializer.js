2d1fc36a6dd65504fa2d474024dac2b4ee5ad450
Seena <seenarowhani95@gmail.com>
2017-03-13 14:54:45 -0400

    refactor(mixin): Remove base64 encoding in favour of stringifed json

--- addon/mixins/ember-qp-serializer.js
+++ addon/mixins/ember-qp-serializer.js
@@ -1,13 +1,24 @@
 import Ember from 'ember'
 const {Mixin, Logger} = Ember
-const TAG_NAME = 'ember-qp-serializer'
+
+const TAG_NAME = '[ember-qp-serializer]'
 const VALID_TYPES = ['object', 'instance']
+
 export default Mixin.create({
+  /**
+   * serializeQueryParam
+   * For each `queryParams` specified in controller, it will
+   * serialize them, and ship them off to the router.
+   * @param  {any} value The value to be serialized
+   * @param  {string} urlKey qp key referencing `value`
+   * @param  {string} defaultValueType [description]
+   * @return {string} serialized query parameter
+   */
   serializeQueryParam (value, urlKey, defaultValueType) {
     if (VALID_TYPES.includes(defaultValueType)) {
-      let result = {}
+      let result = ''
       try {
-        result = btoa(JSON.stringify(value))
+        result = JSON.stringify(value)
       } catch (e) {
         Logger.warn(TAG_NAME, e)
       }
@@ -16,12 +27,18 @@ export default Mixin.create({
       return this._super(...arguments)
     }
   },
-
+  /**
+   * deserializeQueryParam
+   * @param  {string} value The value to be deserialized
+   * @param  {string} urlKey qp key referencing `value`
+   * @param  {string} defaultValueType [description]
+   * @return {any} Deserialized query parameters
+   */
   deserializeQueryParam (value, urlKey, defaultValueType) {
     if (VALID_TYPES.includes(defaultValueType)) {
       let result = {}
       try {
-        result = JSON.parse(atob(value))
+        result = JSON.parse(value)
       } catch (e) {
         Logger.warn(TAG_NAME, e)
       }
